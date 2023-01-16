9a4ca58169f568180a86cd3ff9e12c513c30d9a1
Asus <kuba.wiechnik23@gmail.com>
2023-01-16 13:50:01 +0100

    A strange change in the classes_def.xml file makes the code compile

--- OmtfDataFormats/interface/GenObj.h
+++ OmtfDataFormats/interface/GenObj.h
@@ -7,7 +7,7 @@ class GenObj : public TObject {
 public:
  GenObj(float pt=0., float eta=0., float phi=0.,float mass=0., 
 	int charge=0, int pdgid=0, int st=0, int mother=0, double vx=0):
-  _pt(pt),_eta(eta),_phi(phi),_mass(mass),_charge(charge),_id(pdgid),_status(st),_mid(mother), _vx(vx) {}
+  _pt(pt),_eta(eta),_phi(phi),_mass(mass),_charge(charge),_id(pdgid),_status(st),_mid(mother),_vx(vx) {}
   virtual ~GenObj() {}
 public:
   float pt() const { return _pt;}
@@ -18,7 +18,7 @@ public:
   int status() const { return _status;}
   int motherId() const { return _mid;}
   int charge() const { return _charge;}
-  int vx() const { return _vx;}
+  double vx() const { return _vx;}
 
 
 private:  
@@ -27,7 +27,7 @@ private:
   double _vx;
 
 public:
-  ClassDef(GenObj,1);
+  ClassDef(GenObj,2);
 };
 
 std::ostream & operator<< (std::ostream &out, const GenObj &o);
--- OmtfDataFormats/src/GenObj.cc
+++ OmtfDataFormats/src/GenObj.cc
@@ -10,7 +10,7 @@ std::ostream & operator<< (std::ostream &out, const GenObj &o)
   out<<" charge: "<<o.charge();
   out<<"pt: "<<o.pt()<<" eta: "<<o.eta()<<" phi: "<<o.phi();
   out<<" status: "<<o.status()<<" motherId: "<<o.motherId();
-  out<<" x coordinate of vertex position: "<<o.vx;
+  // out<<" x coordinate of vertex position: "<<o.vx();
 
   out<<std::endl;
 
