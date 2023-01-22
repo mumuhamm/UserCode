acbb3cebc9f5769297d53c0e68232ddd74a9ed96
Asus <kuba.wiechnik23@gmail.com>
2023-01-22 19:09:25 +0100

    Working program with beta added

--- OmtfDataFormats/interface/GenObj.h
+++ OmtfDataFormats/interface/GenObj.h
@@ -8,8 +8,8 @@
 class GenObj : public TObject {
 public:
  GenObj(float pt=0., float eta=0., float phi=0.,float mass=0., 
-	int charge=0, int pdgid=0, int st=0, int mother=0, double vx=0., double vy=0., double vz=0.):
-  _pt(pt),_eta(eta),_phi(phi),_mass(mass),_charge(charge),_id(pdgid),_status(st),_mid(mother),_vx(vx),_vy(vy),_vz(vz){}
+	int charge=0, int pdgid=0, int st=0, int mother=0, double vx=0., double vy=0., double vz=0., double beta=0):
+  _pt(pt),_eta(eta),_phi(phi),_mass(mass),_charge(charge),_id(pdgid),_status(st),_mid(mother),_vx(vx),_vy(vy),_vz(vz),_beta(beta){}
   virtual ~GenObj() {}
 public:
   float pt() const { return _pt;}
@@ -23,15 +23,16 @@ public:
   double vx() const { return _vx;}
   double vy() const { return _vy;}
   double vz() const { return _vz;}
+  double beta() const { return _beta;}
 
 
 private:  
   float _pt,_eta,_phi,_mass; 
   int _charge,_id,_status,_mid; 
-  double _vx, _vy, _vz;
+  double _vx, _vy, _vz, _beta;
 
 public:
-  ClassDef(GenObj,3);
+  ClassDef(GenObj,4);
 };
 
 std::ostream & operator<< (std::ostream &out, const GenObj &o);
