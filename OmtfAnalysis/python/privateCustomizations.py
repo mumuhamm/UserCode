6554de4ec45738b2b4dfb55b9535e7be3d673d08
Artur Kalinowski <Artur.Kalinowski@cern.ch>
2023-02-13 16:43:45 +0100

    * improve products list for the GMT/OMTF studies

--- OmtfAnalysis/python/privateCustomizations.py
+++ OmtfAnalysis/python/privateCustomizations.py
@@ -18,20 +18,20 @@ def customize_L1TkMuonsGmt(process):
 def customize_outputCommands(process):
     process.FEVTSIMoutput.outputCommands.extend(cms.untracked.vstring('drop *',
                                                                       'keep *_genParticles_*_*',
-                                                                      'keep *_mix_*_*',                                                                      
+                                                                      'keep *Track*_mix_*_*',                                                                                                                                        
                                                                       'keep *_prunedTrackingParticles_*_*',
                                                                       'keep *_generalTracks_*_*',
-                                                                      'keep *_MergedTrackTruth_*_*',                                                                     
+                                                                      'keep *_MergedTrackTruth_*_*',  
                                                                       'keep *_simMuonDTDigis_*_*',
                                                                       'keep *_simMuonCSCDigis_*_*',
                                                                       'keep *_simMuonRPCDigis_*_*',
                                                                       'keep *_simBmtfDigis_*_*',
                                                                       'keep *_simKBmtfDigis_*_*',
                                                                       'keep *_simOmtfDigis_*_*',
-                                                                      'keep *_simEmtfDigis_*_*',
-                                                                      'keep *_simCscTriggerPrimitiveDigis_*_*',
-                                                                      'keep *_simDtTriggerPrimitiveDigis_*_*',                                                                      
+                                                                      'keep *_simEmtfDigis_*_*',   
                                                                       'keep *_simGmtStage2Digis_*_*',
+                                                                      'keep *_simCscTriggerPrimitiveDigis_*_*',
+                                                                      'keep *_simDtTriggerPrimitiveDigis_*_*',  
                                                                       'keep *_dtTriggerPhase2PrimitiveDigis_*_*',                                                                       
                                                                       'keep *_l1tTTTracksFromExtendedTrackletEmulation_*_*',
                                                                       'keep *_l1tTTTracksFromTrackletEmulation_*_*',
@@ -50,7 +50,7 @@ def customize_outputCommands(process):
 ##########################################
 ##########################################
 def customize_extra_outputCommands(process):
-    process.FEVTSIMoutput.outputCommands.extend(cms.untracked.vstring(
+    process.FEVTSIMoutput.outputCommands.extend(cms.untracked.vstring(                                                                                                                      
                                                                       'keep *_g4SimHits_MuonCSCHits_*',
                                                                       'keep *_g4SimHits_MuonDTHits_*',
                                                                       'keep *_g4SimHits_MuonGEMHits_*',
